861ee0e866ad3f33dce59d0043c67ad6e352da12
Jared Magrath <jared.magrath@newsignature.com>
2020-08-15 00:29:50 +0100

    setting up wider application

---
+++ notebooks/Configuration/Schemas.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+schema = StructType([
+  StructField("EnqueuedTimeUtc", StringType(), True),
+  StructField("Body", BinaryType(), True),
+  StructField("Properties", MapType(StringType(), StructType([
+    StructField("member0", LongType(), True),
+    StructField("member1", DoubleType(), True),
+    StructField("member2", StringType(), True),
+    StructField("member3", BinaryType(), True)
+  ])), True)
+])
+
+# COMMAND ----------
+
